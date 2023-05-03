fe8684501cb7d852a82c0fec9a1bfb1214a273a9
tsurkanoleg <tsurkanoleg1993@gmail.com>
2023-05-03 22:35:50 +0300

    icon

--- src/route/index.js
+++ src/route/index.js
@@ -1869,6 +1869,16 @@ router.get('/bootstrapform', function (req, res) {
 
 // ================================================================
 
+router.get('/bootstrapicon', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrapicon', {
+    layout:"basic",
+
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
 
 
 
